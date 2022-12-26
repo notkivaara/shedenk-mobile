6f819b047bacf36e8113f3a1cd150d6421b63f87
notkivaara <putrastbpb@gmail.com>
2022-12-26 19:33:59 +0700

    stash sebentar

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,13 +1,61 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import '../../../models/product.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   final Product product;
   const Body({Key? key, required this.product}) : super(key: key);
 
   @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+
+  List<Widget> myData = [
+    Image(image: NetworkImage("https:picsum.photos/350/500")),
+    Image(image: NetworkImage("https:picsum.photos/350/500")),
+    Image(image: NetworkImage("https:picsum.photos/350/500")),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Column(children: [SingleChildScrollView()]);
+    return Column(children: [
+      CarouselSlider(
+        items: myData,
+        carouselController: _controller,
+        options: CarouselOptions(
+            autoPlay: true,
+            enlargeCenterPage: true,
+            aspectRatio: 2.0,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _current = index;
+              });
+            }),
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: myData.asMap().entries.map((entry) {
+          return GestureDetector(
+            onTap: () => _controller.animateToPage(entry.key),
+            child: Container(
+              width: 12.0,
+              height: 12.0,
+              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: (Theme.of(context).brightness == Brightness.dark
+                          ? Colors.white
+                          : Colors.black)
+                      .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+            ),
+          );
+        }).toList(),
+      ),
+    ]);
   }
 }
