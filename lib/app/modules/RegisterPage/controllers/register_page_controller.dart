c3667ad6c4cc279f2bb81532fb514c3fd0becd45
fredaadifardana <fredadifar@gmail.com>
2022-12-31 15:44:23 +0700

    API login, API register

--- lib/app/modules/RegisterPage/controllers/register_page_controller.dart
+++ lib/app/modules/RegisterPage/controllers/register_page_controller.dart
@@ -8,4 +8,5 @@ class RegisterPageController extends GetxController {
   final TelpController = TextEditingController();
   final PasswordOneController = TextEditingController();
   final PasswordTwoController = TextEditingController();
+  final ProfileImageController = TextEditingController();
 }
